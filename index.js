652b3117d275e8e7e68a1d5660f815a40d10cbe0
Marcin Szamotulski <mszamot@gmail.com>
2016-10-08 15:59:59 +0100

    version 0.0.5: MaybeM, PromiseEitherM, Either
    Add mocha tests.

--- index.js
+++ index.js
@@ -35,21 +35,21 @@ function do_(monad, doG, cb, stack=[]) {
     return (function inner(stack, args) {
       const {value, done} = args,
         { monad, doBlock }= stack[stack.length - 1]
-      console.log(`${monad.name}: ${JSON.stringify(args)} ${stack.length}`)
-      debugger
+      // console.log(`${monad.name}: ${JSON.stringify(args)} ${stack.length}`)
       if (!done)
         return monad.join(stack, unwrap(value, stack.length), inner.bind(null, stack))
       // here I should deal with return values (which should by of type Monad)
       else if (stack.length <= 1) {
-        console.log(`value: ${value}, ${stack.length}`)
+        // console.log(`value: ${JSON.stringify(value)}, ${stack.length}`)
         return cb(unwrap(value, stack.length - 1))
       }
     })(stack, {value, done})
   } else {
-    return (function* innerG(stack) {
+    return (function* wrapper(stack) {
       stack.push({ monad , doBlock })
       let result = yield* doBlock
       stack.pop()
+      // console.log(`wrapper ${JSON.stringify(result)}`)
       return result
     })(stack)
   }
@@ -59,136 +59,43 @@ class MaybeM {
   static return(value) {return value}
   static fail(error) {return null}
   static join(stack, value, cb) {
-    console.log(`MaybeM.join(${stack.length}, ${JSON.stringify(value)})`)
+    // console.log(`MaybeM.join(${stack.length}, ${JSON.stringify(value)})`)
     if (value !== null) {
       const { doBlock } = stack[0]
       cb(doBlock.next(wrap(value, stack.length)))
     } else {
       const { doBlock } = stack[stack.length - 1]
-      if (stack.length > 1) {
-        doBlock.next({ value: null, done: true })
-        cb(stack[0].doBlock.next({ value: null, done: false }))
-      } else
-        cb(doBlock.next({ value: null, done: true }))
+      doBlock.next({ value: null, done: true })
+      cb(stack[0].doBlock.next({ value: null, done: false }))
     }
   }
 }
 
-class PromiseM {
-  static return(value) {return Promise.resolve(value)}
-  static fail(error) {return Promise.resolve(null)}
+class Either extends Array {
+  constructor(left, right) {
+    super(left, right)
+  }
+}
+
+class PromiseEitherM {
   static join(stack, promise, cb) {
     promise
       .then(value => {
         const { doBlock } = stack[0]
-        console.log(`PromiseM: then: doBlock.next(${JSON.stringify(wrap(value, stack.length))})`)
-        cb(doBlock.next(wrap(value, stack.length)))
+        // console.log(`PromiseEitherM: then: doBlock.next(${JSON.stringify(wrap(value, stack.length))})`)
+        cb(doBlock.next(wrap(new Either(null, value), stack.length)))
       })
       .catch(err => {
         const { doBlock } = stack[stack.length - 1]
-        console.log(`PromiseM: catch: ${JSON.stringify(err)}`)
-        if (stack.length > 1) {
-          doBlock.next({ value: Promise.reject(null), done: true})
-          cb(stack[0].doBlock.next({ value: Promise.reject(null), done: false}))
-        } else
-          cb(doBlock.next({ value: Promise.reject(null), done: true}))
+        // console.log(`PromiseEitherM: catch: ${JSON.stringify(err)}`)
+        const value = new Either(err, null)
+        doBlock.next({ value, done: true})
+        cb(stack[0].doBlock.next({ value, done: false}))
       })
   }
 }
 
 exports.do = do_
-
-function* maybeComp() {
-  const x = yield 1
-  const y = yield 2
-  const z = yield 3
-  console.log('maybeComp:', x, y, z)
-  return x + y + z
-}
-
-function* maybeCompFail() {
-  const x = yield 1
-  const y = yield null
-  const z = yield 3
-  console.log('maybeCompFail:', x, y, z)
-  return x + y + z
-}
-
-function* promiseComp() {
-  const x = yield new Promise((resolve, reject) => {
-    setTimeout(resolve.bind(null, 1), 200)
-  })
-  const y = yield new Promise((resolve, reject) => {
-    setTimeout(resolve.bind(null, 2), 200)
-  })
-  const z = yield new Promise((resolve, reject) => {
-    setTimeout(resolve.bind(null, 3), 200)
-  })
-  console.log(`promiseComp.return: ${x} ${y} ${z}`)
-  return Promise.resolve(x + y + z)
-}
-
-function* promiseCompFail() {
-  const x = yield new Promise((resolve, reject) => {
-    setTimeout(resolve.bind(null, 1), 200)
-  })
-  const y = yield new Promise((resolve, reject) => {
-    setTimeout(reject.bind(null, new Error('ups...')), 200)
-  })
-  const z = yield new Promise((resolve, reject) => {
-    setTimeout(resolve.bind(null, 3), 200)
-  })
-  return Promise.resolve(x + y + z)
-}
-
-if (require.main === module) {
-  // do_(MaybeM, maybeComp, v => console.log(`maybeComp: ${JSON.stringify(v)}`))
-  // console.log('\n')
-  // do_(MaybeM, maybeCompFail, v => console.log(`maybeComp: ${JSON.stringify(v)}`))
-  // console.log('\n')
-
-  // do_(PromiseM, promiseComp, promise => promise.then((v) => console.log(`promiseComp: ${v}`)).catch((e) => console.log(`promiseComp: error ${e}`)))
-  // do_(PromiseM, promiseCompFail, promise => promise.then((v) => console.log(`promiseCompFail: ${v}`)).catch((e) => console.log(`promiseCompFail: ${e}`)))
-  
-   
-    /*
-     * do_(MaybeM, function* (stack) {
-     *   let a = yield 0
-     *   console.log(`a=${a}`)
-     *   let b = yield* do_(MaybeM, maybeCompFail, null, stack)
-     *   console.log(`b=${JSON.stringify(b)}`)
-     *   let c = yield 4
-     *   console.log(`c=${c}`)
-     *   return [a, b, c]
-     * }, val => console.log(`val: ${JSON.stringify(val)}`))
-     */
-
-
-    // TODO: return Promise -> deal with return values
-    
-     // do_(MaybeM, function* (stack) {
-       // const x = yield -1
-       // const y = yield 0
-       // const z = yield* do_(PromiseM, promiseComp, null, stack)
-       // console.log(`z ${JSON.stringify(z)} ${z && z.constructor ? z.constructor.name : z}`)
-       // const a = yield 4
-       // const b = yield 5
-       // return z.then(z => [x, y, z, a, b])
-     // }, p => {
-       // if (p)
-         // p
-           // .then(v => console.log(`mixed (then): ${v}`))
-           // .catch(e => console.log(`mixed (catch): ${e}`));
-       // else
-         // console.log(`mixed ?: ${p}`);
-     // })
-
-  /*
-   * do_(PromiseM, function* (stack) {
-   *   const x = yield new Promise((resolve, _) => setTimeout(resolve.bind(null, 1), 200))
-   *   const y = yield new Promise((resolve, _) => setTimeout(resolve.bind(null, 2), 200))
-   *   const z = yield* do_(MaybeM, maybeComp, null, stack)
-   *   return x + y + z
-   * }, v => console.log(`mixed': ${v}`))
-   */
-}
+exports.Either = Either
+exports.MaybeM = MaybeM
+exports.PromiseEitherM = PromiseEitherM
